bc62f1fab9a1d2dbb46c2be982afb4f954ed38ed
CreativForm <creativform@gmail.com>
2021-04-21 22:10:19 +0200

    New search functionality

--- inc/Search.php
+++ inc/Search.php
@@ -21,9 +21,94 @@ class Serbian_Transliteration_Search extends Serbian_Transliteration
      */
     public function __construct()
     {
-        $this->add_filter( 'request', 'request' );
-		$this->add_filter( 'get_search_query', 'request' );
+//		$this->add_filter( 'request', 'request' );
+//		$this->add_filter( 'get_search_query', 'request' );
+		$this->add_filter('posts_search', 'posts_search', 500, 2);
     }
+	
+	
+	/**
+     * Special WP_Query Transliteration Search
+	 *
+	 * @author            Ivijan-Sefan Stipic
+	 * @since             1.6.0
+	 * @version           1.0.0
+     */
+	public function posts_search( $search, $wp_query )
+	{
+		global $wpdb;
+		
+		if(empty($search)) {
+			return $search; // skip processing - no search term in query
+		}
+		
+		$q = $wp_query->query_vars;
+		
+		$n = !empty($q['exact']) ? '' : '%';
+		$search = $searchand = '';
+		
+		$q['search_orderby_title'] = array();
+
+		$exclusion_prefix = apply_filters( 'wp_query_search_exclusion_prefix', '-' );
+
+		foreach ( $q['search_terms'] as $term ) {
+			
+			$term_transliterated = $this->transliterate_text(
+				(get_rstr_option('fix-diacritics', 'no') == 'yes' ? $this->fix_diacritics($term) : $term),
+				(get_rstr_option('site-script', 'lat') == 'cyr' ? 'lat_to_cyr' : 'cyr_to_lat')
+			);
+			
+			// If there is an $exclusion_prefix, terms prefixed with it should be excluded.
+			$exclude = $exclusion_prefix && ( substr( $term, 0, 1 ) === $exclusion_prefix );
+			
+			if ( $exclude ) {
+				$like_op  = 'NOT LIKE';
+				$andor_op = 'AND';
+				$term     = substr( $term, 1 );
+				$term_transliterated     = substr( $term_transliterated, 1 );
+			} else {
+				$like_op  = 'LIKE';
+				$andor_op = 'OR';
+			}
+			
+			if ( $n && !$exclude ) {
+				$like							= '%' . $wpdb->esc_like( $term ) . '%';
+				$q['search_orderby_title'][]	= $wpdb->prepare( "{$wpdb->posts}.post_title LIKE %s", $like );
+				
+				$like							= '%' . $wpdb->esc_like( $term_transliterated ) . '%';
+				$q['search_orderby_title'][]	= $wpdb->prepare( "{$wpdb->posts}.post_title LIKE %s", $like );
+			}
+			
+			$like      = $n . $wpdb->esc_like( $term ) . $n;
+			$tr_like      = $n . $wpdb->esc_like( $term_transliterated ) . $n;
+			$search   .= $wpdb->prepare("{$searchand}(
+				(
+					({$wpdb->posts}.post_title {$like_op} %s)
+				{$andor_op}
+					({$wpdb->posts}.post_excerpt {$like_op} %s)
+				{$andor_op}
+					({$wpdb->posts}.post_content {$like_op} %s)
+				)
+				OR
+				(
+					({$wpdb->posts}.post_title {$like_op} %s)
+				{$andor_op}
+					({$wpdb->posts}.post_excerpt {$like_op} %s)
+				{$andor_op}
+					({$wpdb->posts}.post_content {$like_op} %s)
+				)
+			)", $like, $like, $like, $tr_like, $tr_like, $tr_like);
+			$searchand = ' AND ';
+		}
+		
+		if (!empty($search)) {
+			$search = " AND ({$search}) ";
+			if (!is_user_logged_in())
+				$search .= " AND ({$wpdb->posts}.post_password = '') ";
+		}
+		
+		return $search;
+	}
 	
 	public function request ($search_vars) {
 		if ( isset($search_vars['s']) && !empty($search_vars['s']) ) {
